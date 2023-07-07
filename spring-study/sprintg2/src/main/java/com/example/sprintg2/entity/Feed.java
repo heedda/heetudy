74270b9901f4dc4e1ba2ebe8c92780c8ae5231f8
heedda <heedda.kollim@gmail.com>
2023-07-07 20:29:45 +0900

    Spring CRUD만들기

--- spring-study/sprintg2/src/main/java/com/example/sprintg2/entity/Feed.java
+++ spring-study/sprintg2/src/main/java/com/example/sprintg2/entity/Feed.java
@@ -1,8 +1,36 @@
 package com.example.sprintg2.entity;
 
-import lombok.Getter;
+import com.example.sprintg2.controller.dto.request.UpdateFeedRequest;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class Feed {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "BIGINT", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(columnDefinition = "VARCHAR(20)")
+    private String title;
+
+    @NotNull
+    @Column(columnDefinition = "VARCHAR(10000)")
+    private String content;
+
+    @Builder
+    public Feed(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    public void updateFeed(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 
 }
